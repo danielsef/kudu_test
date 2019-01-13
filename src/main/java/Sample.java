0f9ca766f54e2113eec42f63527bbad9d68f6fa3
Sef Dániel <sefdeni@gmail.com>
2019-01-14 00:12:10 +0100

    Initial commit

---
+++ src/main/java/Sample.java
@@ -0,0 +1,91 @@
+import org.apache.kudu.ColumnSchema;
+import org.apache.kudu.Schema;
+import org.apache.kudu.Type;
+import org.apache.kudu.client.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+
+    private static final String KUDU_MASTER = System.getProperty(
+            "kuduMaster", "192.168.60.100");
+
+    public static void main(String[] args) {
+        System.out.println("-----------------------------------------------");
+        System.out.println("Will try to connect to Kudu master at " + KUDU_MASTER);
+        System.out.println("Run with -DkuduMaster=myHost:port to override.");
+        System.out.println("-----------------------------------------------");
+        String tableName = "java_sample-" + System.currentTimeMillis();
+        KuduClient client = new KuduClient.KuduClientBuilder(KUDU_MASTER).build();
+
+        try {
+
+
+            //CREATE TABLE
+            List<ColumnSchema> columns = new ArrayList(2);
+            columns.add(new ColumnSchema.ColumnSchemaBuilder("key", Type.INT32)
+                    .key(true)
+                    .build());
+            columns.add(new ColumnSchema.ColumnSchemaBuilder("value", Type.STRING)
+                    .build());
+            List<String> rangeKeys = new ArrayList<>();
+            rangeKeys.add("key");
+
+            Schema schema = new Schema(columns);
+            System.out.println("columns: " + schema.getColumns().toString());
+
+            CreateTableOptions options = new CreateTableOptions()
+                    .setRangePartitionColumns(rangeKeys)
+                    .setNumReplicas(1);
+            client.createTable(tableName,schema,options);
+            System.out.println("Table name: " + tableName);
+
+            //INSERT
+            KuduTable table = client.openTable(tableName);
+            KuduSession session = client.newSession();
+            for (int i = 0; i < 3; i++) {
+                Insert insert = table.newInsert();
+                PartialRow row = insert.getRow();
+                row.addInt(0, i);
+                row.addString(1, "value " + i);
+                session.apply(insert);
+            }
+
+
+            List<String> projectColumns = new ArrayList<>(1);
+            projectColumns.add("key");
+            projectColumns.add("value");
+            KuduScanner scanner = client.newScannerBuilder(table)
+                    .setProjectedColumnNames(projectColumns)
+                    .build();
+            while (scanner.hasMoreRows()) {
+                RowResultIterator results = scanner.nextRows();
+                while (results.hasNext()) {
+                    RowResult result = results.next();
+                    System.out.println(result.getInt(0));
+                    System.out.println(result.getString(1));
+
+                    //System.out.println(result.toString());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+
+                //DELETE
+                //client.deleteTable(tableName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    client.shutdown();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
